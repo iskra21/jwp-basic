ee3a416343c7de3115f0adaa98eb5cbb29ce93ee
iskra <iskra@DESKTOP-7D22UII>
2022-02-19 17:42:58 +0900

    login 기능 완성함.

---
+++ src/main/java/next/web/LogoutServlet.java
@@ -0,0 +1,31 @@
+package next.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+@WebServlet("/user/logout")
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    private static final Logger log = LoggerFactory.getLogger(CreateUserServlet.class);
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session = req.getSession();
+		session.invalidate();
+		resp.sendRedirect("/index.jsp");
+	}
+
+}
