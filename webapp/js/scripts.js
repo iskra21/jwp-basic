d55b7bb55d5ba768bf3bbdd29568095c03a1d66d
iskra <iskra@DESKTOP-7D22UII>
2022-03-13 21:03:01 +0900

    답변하기 구현함

--- webapp/js/scripts.js
+++ webapp/js/scripts.js
@@ -1,3 +1,30 @@
+$(".answerWrite input[type=submit]").click(addAnswer);
+
+function addAnswer(e) {
+	e.preventDefault();
+	
+	var queryString = $("form[name=answer]").serialize();
+	
+	$.ajax({
+		type : 'post',
+		url : '/api/qna/addAnswer',
+		data : queryString,
+		dataType : 'json',
+		error : onError,
+		success : onSuccess,
+	});
+}
+
+function onSuccess(json, status){
+  var answerTemplate = $("#answerTemplate").html();
+  var template = answerTemplate.format(json.writer, new Date(json.createdDate), json.contents, json.answerId, json.answerId);
+  $(".qna-comment-slipp-articles").prepend(template);
+}
+
+function onError(xhr, status) {
+  alert("error");
+}
+
 String.prototype.format = function() {
   var args = arguments;
   return this.replace(/{(\d+)}/g, function(match, number) {
@@ -7,4 +34,3 @@ String.prototype.format = function() {
         ;
   });
 };
-
