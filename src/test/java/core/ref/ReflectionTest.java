85366e739aca25affa0f54ebf549d116d72597be
FSC <FSC@172.16.139.103>
2022-04-05 08:50:46 +0900

    리플렉션으로부터 필드, 생성자, 메소드의 모든 정보를 얻어옴

--- src/test/java/core/ref/ReflectionTest.java
+++ src/test/java/core/ref/ReflectionTest.java
@@ -1,5 +1,9 @@
 package core.ref;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +18,30 @@ public class ReflectionTest {
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+        Field[] fields = clazz.getDeclaredFields();
+        if (fields.length != 0) {
+        	int i = 0;
+        	for (Field field:fields) {
+        		System.out.println("Field["+ i++ +"]: " + field.getType().getTypeName() + " " + field.getName());
+        	}
+        }
+        
+        Constructor[] constructors = clazz.getDeclaredConstructors();
+        if (constructors.length != 0) {
+        	int i = 0;
+        	for (Constructor cons:constructors) {
+        		System.out.println("Constructor["+ i++ +"]: " + cons.getName());
+        	}
+        }
+        
+        Method[] meds = clazz.getDeclaredMethods();
+        if (meds.length != 0) {
+        	int i = 0;
+        	for (Method med:meds) {
+        		System.out.println("Method["+ i++ +"]: " + med.getReturnType().getTypeName() + " " + med.getName());
+        	}        	
+        }
+        
     }
     
     @Test
