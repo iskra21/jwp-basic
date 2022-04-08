5a63308ecdd00e610c2aa40e6d55532d3c80ed95
FSC <FSC@172.16.139.103>
2022-04-08 13:19:24 +0900

    메소드에서 리플렉션 얻기 성공

--- src/test/java/core/ref/Junit4TestRunner.java
+++ src/test/java/core/ref/Junit4TestRunner.java
@@ -1,19 +1,28 @@
 package core.ref;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.ElementType;
 import java.lang.reflect.Method;
 
 import org.junit.Test;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@interface MyTest {
+	public int value() default 0;
+}
 
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-        Method[] methods = clazz.getDeclaredMethods();
-        Junit4Test temp;
-        for (Method method:methods) {
-        	temp = method.getAnnotation(clazz);
-        	System.out.println(temp.getName());
-        	method.invoke(clazz.newInstance());
+        for (Method method : clazz.getDeclaredMethods()) {
+        	MyTest test = method.getDeclaredAnnotation(MyTest.class);
+        	if (test != null) {
+        		method.invoke(clazz.newInstance());
+        	}
         }
     }
 }
