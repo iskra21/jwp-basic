8b9b326e695be3907033e75d2e19ddfda3f39b89
FSC <FSC@172.16.139.103>
2022-04-08 09:11:34 +0900

    아노테이션 실험중

--- src/test/java/core/ref/Junit4TestRunner.java
+++ src/test/java/core/ref/Junit4TestRunner.java
@@ -1,11 +1,19 @@
 package core.ref;
 
+import java.lang.reflect.Method;
+
 import org.junit.Test;
 
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
-
+        Method[] methods = clazz.getDeclaredMethods();
+        Junit4Test temp;
+        for (Method method:methods) {
+        	temp = method.getAnnotation(clazz);
+        	System.out.println(temp.getName());
+        	method.invoke(clazz.newInstance());
+        }
     }
 }
