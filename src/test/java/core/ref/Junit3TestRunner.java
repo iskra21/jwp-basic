9f54fe63e84d0463c7fb96b3bf677769407b1ef0
FSC <FSC@172.16.139.103>
2022-05-03 08:59:57 +0900

    일부 수정

--- src/test/java/core/ref/Junit3TestRunner.java
+++ src/test/java/core/ref/Junit3TestRunner.java
@@ -8,9 +8,8 @@ public class Junit3TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
-        Method[] methods = clazz.getDeclaredMethods();
         
-        for (Method method:methods) {
+        for (Method method : clazz.getDeclaredMethods() ) {
         	if (method.getName().startsWith("test")) {
         		method.invoke(clazz.newInstance());
         	}
